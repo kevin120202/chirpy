1b38a23e08249357f1ebf7c82057d1a576c654ef
Kevin Dela Paz <kevindelapaz21@gmail.com>
2025-02-21 11:41:41 -0600

    Refactored file structure

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 	"sync/atomic"
 )
@@ -10,45 +10,26 @@ type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request) {
-			cfg.fileserverHits.Add(1)
-
-			next.ServeHTTP(w, r)
-		},
-	)
-}
-
-func (cfg *apiConfig) writeNumOfRequest(w http.ResponseWriter, r *http.Request) {
-	count := cfg.fileserverHits.Load()
-	msg := fmt.Sprintf("Hits: %d", count)
-	w.Write([]byte(msg))
-}
-
-func (cfg *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
-	cfg.fileserverHits.Store(0)
-}
-
-func readiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(200)
-	w.Write([]byte("OK"))
-}
-
 func main() {
-	mux := http.NewServeMux()
-	apiCfg := &apiConfig{}
+	const filepathRoot = "."
+	const port = "8080"
 
-	mux.HandleFunc("GET /healthz", readiness)
-	mux.HandleFunc("GET /metrics", apiCfg.writeNumOfRequest)
-	mux.HandleFunc("POST /reset", apiCfg.reset)
-	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+	apiCfg := &apiConfig{}
+	mux := http.NewServeMux()
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", fsHandler)
+	
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.reset)
+	mux.HandleFunc("POST /api/validate_chirp", handlerChiprsValidate)
 
 	server := &http.Server{
-		Addr: ":8080",
+		Addr: ":" + port,
 		Handler: mux,
 	}
 
-	server.ListenAndServe()
+	log.Printf("Serving files from %s on port:: %s\n", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
 }
+
